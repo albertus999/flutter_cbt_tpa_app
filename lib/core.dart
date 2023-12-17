a328a6f93e5516ca14dff9afd8ceb9659ce223c3
albertus999 <albertus_arifin@yahoo.com>
2023-12-17 22:02:05 +0700

    update 20231217

---
+++ lib/core.dart
@@ -0,0 +1,13 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:flutter_cbt_tpa_app/core/assets/assets.gen.dart';
+export 'package:flutter_cbt_tpa_app/core/components/buttons.dart';
+export 'package:flutter_cbt_tpa_app/core/components/button_circle.dart';
+export 'package:flutter_cbt_tpa_app/core/components/custom_scaffold.dart';
+export 'package:flutter_cbt_tpa_app/core/components/custom_text_field.dart';
+export 'package:flutter_cbt_tpa_app/core/components/search_input.dart';
+export 'package:flutter_cbt_tpa_app/core/constants/colors.dart';
+export 'package:flutter_cbt_tpa_app/core/extensions/build_context_ext.dart';
